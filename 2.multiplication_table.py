6d3c910a98640d999b7d96e852498f998c31bc5a
sai saranya atti <52907670+saisaranyaatti@users.noreply.github.com>
2020-08-17 13:55:05 +0530

    Add files via upload

---
+++ 2.multiplication_table.py
@@ -0,0 +1,26 @@
+#printin multiplication table using for loop
+n=int(input("enter a number"))
+r1=int(input("enter starting range"))
+r2=int(input("enter ending range"))
+if r1<r2:
+    for i in range(r1,r2+1,1):
+        print(n,"*",i,"=",n*i)
+else:
+    for i in range(r1,r2-1,-1):
+        print(n,"*",i,"=",n*i)
+
+
+
+#printing multiplication table using while loop
+n=int(input("enter a number"))
+r1=int(input("enter starting range"))
+r2=int(input("enter ending range"))
+if r1<r2:
+    while r1<=r2:
+        print(n,"*",r1,"=",n*r1)
+        r1+=1
+else:
+    while r1>=r2:
+        print(n,"*",r1,"=",n*r2)
+        r1-=1
+
