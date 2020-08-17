6ac279baa8b3874b0a2dc0c3b6264510d4cd2a88
sai saranya atti <52907670+saisaranyaatti@users.noreply.github.com>
2020-08-17 13:56:37 +0530

    Update 1.prime number.py

--- 1.prime number.py
+++ 1.prime number.py
@@ -1,11 +1,11 @@
 # checking whether the given number is prime or not by using for loop
-'''n=int(input("enter a number"))
+n=int(input("enter a number"))
 for i in range(2,int(n**0.5)+1):
     if n%i==0:
         print(n,"is not a prime number")
         break
 else:
-    print(n,"is a prime number")'''
+    print(n,"is a prime number")
 #checking whether the given number is prime or not by using while loop
 n=int(input("enter a number"))
 i=2
