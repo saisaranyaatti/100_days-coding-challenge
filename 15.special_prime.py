fb37ee70a7d95fcc013cb7a0a622f263f1724f9d
sai saranya atti <52907670+saisaranyaatti@users.noreply.github.com>
2020-08-28 20:39:51 +0530

    Add files via upload

---
+++ 15.special_prime.py
@@ -0,0 +1,35 @@
+import math
+def prime(n):
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            break
+    else:
+        return n
+def nearest_prime(n):
+    i=n-1
+    while True:
+        if prime(i):
+            return i
+        else:
+            i-=1
+    
+def special_prime(n):
+    if prime(n)==n:
+        a=nearest_prime(n)
+        b=nearest_prime(a)
+        while True:
+            if a+b+1==n:
+                print("special_prime")
+                break
+            else:
+                a=b
+                b=nearest_prime(a)
+            if a==2 or b==2:
+                print("not special prime")
+                break
+    else:
+        print("not prime")
+n=int(input())
+special_prime(n)
+            
+            
