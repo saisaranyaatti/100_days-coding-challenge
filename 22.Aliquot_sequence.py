92a464c596a3333fa3e5665ee8cda9e84e67162c
sai saranya atti <52907670+saisaranyaatti@users.noreply.github.com>
2020-09-10 20:05:18 +0530

    Add files via upload

---
+++ 22.Aliquot_sequence.py
@@ -0,0 +1,32 @@
+#aliquot sequence
+def aliquot_seq(n):
+    print(n,end=" ")
+    while n>1:
+        c=0
+        for i in range(1,int(n//2)+1):
+            if n%i==0:
+                c+=i
+        print(c,end=" ")
+        if n==c:
+            break
+        n=c
+n=int(input())
+aliquot_seq(n)
+
+
+def aliquot_seq1(n):
+    if n==1:
+        return 1
+    else:
+        c=0
+        for i in range(1,int(n//2)+1):
+            if n%i==0:
+                c+=i
+        else:
+            if n!=c:
+                return str(n)+" "+str(aliquot_seq1(c))
+            else:
+                return n
+n=int(input())
+print(aliquot_seq1(n))
+    
