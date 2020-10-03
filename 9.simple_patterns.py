cf09dd9e3be39821fa0b45700efefff65b31853a
sai saranya atti <52907670+saisaranyaatti@users.noreply.github.com>
2020-10-03 22:30:56 +0530

    Update 9.simple_patterns.py

--- 9.simple_patterns.py
+++ 9.simple_patterns.py
@@ -116,7 +116,145 @@ for i in range(n):
     print(chr(i+65)*(i+1))
 
 
+    0
+   101
+  21012
+ 3210123
+432101234
 
+n=int(input())
+x=1
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end='')
+    for k in range(1,x+1):
+        print(abs(k-i),end='')
+    x+=2
+    print()
+
+
+
+    *
+   **
+  ***
+ ****
+*****
+ ****
+  ***
+   **
+    *
+
+
+
+n=int(input())
+for i in range(1,n+1):
+    print(("*"*i).rjust(n))
+for i in range(n-1,0,-1):
+    print(("*"*i).rjust(n))
+    
+    
+*    
+**   
+***  
+**** 
+*****
+**** 
+***  
+**   
+*    
+
+n=int(input())
+for i in range(1,n+1):
+    print(("*"*i).ljust(n))
+for i in range(n-1,0,-1):
+    print(("*"*i).ljust(n))
+    
+    
+    
+    
+    
+
+*   *****
+*   *    
+*   *    
+*   *    
+*********
+    *   *
+    *   *
+    *   *
+*****   *
+
+
+n=int(input())
+for i in range(1,2*n):
+    for j in range(1,2*n):
+        if i==n or j==n:
+            print("*",end='')
+        elif j==1 and i<n:
+            print("*",end='')
+        elif i==1 and j>n:
+            print("*",end='')
+        elif i==2*n-1 and j<n:
+            print("*",end='')
+        elif j==2*n-1 and i>n:
+            print("*",end='')
+        else:
+            print(" ",end='')
+    print()
+    
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for j in range(i):
+        print("* ",end="")
+    print()
+
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    *
+
+    
+n=int(input())
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(n-i):
+        print("* ",end="")
+    print()
+
+    
+    *     
+   * *    
+  * * *   
+ * * * *  
+* * * * * 
+ * * * *  
+  * * *   
+   * *    
+    *     
+    
+n=int(input())
+for i in range(n):
+    print(("* "*(i+1)).center(2*n))
+for j in range(n):
+    print(("* "*(n-j-1)).center(2*n))
+
+n=int(input())
+for i in range(n,-(n),-1):
+    for j in range(abs(i)):
+        print(" ",end="")
+    for j in range(n-abs(i)):
+        print("* ",end="")
+    print()
 
 
 
